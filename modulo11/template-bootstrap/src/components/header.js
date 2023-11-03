237996903306375bdc316f2026f483a7e8b5db72
TrabalhoGh <dgabriel826@gmail.com>
2023-11-03 01:04:35 -0300

    Ajuste: Centralizar Titulo da Pagina

--- modulo11/template-bootstrap/src/components/header.js
+++ modulo11/template-bootstrap/src/components/header.js
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
- 
+
+const centerStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+};
+
 function Header() {
     return (
       <Navbar bg="dark" variant="dark">
-        <Container>
+        <Container style={centerStyle}>
           <Navbar.Brand>Gerenciamento de Projetos</Navbar.Brand>
         </Container>
       </Navbar>
